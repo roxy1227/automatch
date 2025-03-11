466d50cb5185e1cfd1fdb73c39ab2fcbd18b4103
roxy1227 <roxygllt521@gmail.com>
2025-03-12 05:02:39 +0800

    Revert "打包集成+目录整理"
    This reverts commit 3c900735934a2ae1a424f48bd9a8fd1cd645e00c.

--- app/tools/dpi.py
+++
@@ -1,7 +0,0 @@
-import ctypes
-
-def set_dpi_awareness():
-    try:
-        ctypes.windll.shcore.SetProcessDpiAwareness(1)  # 系统级 DPI 感知 [[9]]
-    except Exception as e:
-        print(f"DPI 设置失败: {e}")
